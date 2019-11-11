5447cd7e3415f236cb767756e08f6bd220673d96
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-11-11 16:13:16 -0600

    All Extract methods modernized and completed

--- tableau_rest_api/methods/extract.py
+++ tableau_rest_api/methods/extract.py
@@ -1,34 +1,21 @@
 from .rest_api_base import *
 class ExtractMethods(TableauRestApiBase):
-    def get_extract_refresh_tasks(self):
-        """
-        :rtype: etree.Element
-        """
+    def get_extract_refresh_tasks(self) -> etree.Element:
         self.start_log_block()
         extract_tasks = self.query_resource('tasks/extractRefreshes')
         self.end_log_block()
         return extract_tasks
 
-    def get_extract_refresh_task(self, task_luid):
-        """
-        :type task_luid: unicode
-        :rtype: etree.Element
-        """
+    def get_extract_refresh_task(self, task_luid: str) -> etree.Element:
         self.start_log_block()
         extract_task = self.query_resource('tasks/extractRefreshes/{}'.format(task_luid))
         self.start_log_block()
         return extract_task
 
-    def get_extract_refresh_tasks_on_schedule(self, schedule_name_or_luid):
-        """
-        :param schedule_name_or_luid: unicode
-        :rtype: etree.Element
-        """
+    def get_extract_refresh_tasks_on_schedule(self, schedule_name_or_luid: str):
+
         self.start_log_block()
-        if self.is_luid(schedule_name_or_luid):
-            schedule_luid = schedule_name_or_luid
-        else:
-            schedule_luid = self.query_schedule_luid(schedule_name_or_luid)
+        schedule_luid = self.query_schedule_luid(schedule_name_or_luid)
         tasks = self.get_extract_refresh_tasks()
         tasks_on_sched = tasks.findall('.//t:schedule[@id="{}"]/..'.format(schedule_luid), self.ns_map)
         if len(tasks_on_sched) == 0:
@@ -37,42 +24,25 @@ class ExtractMethods(TableauRestApiBase):
                 "No extract refresh tasks found on schedule {}".format(schedule_name_or_luid))
         self.end_log_block()
 
-    def run_extract_refresh_task(self, task_luid):
-        """
-        :task task_luid: unicode
-        :rtype: unicode
-        """
+    def run_extract_refresh_task(self, task_luid:str) -> str:
         self.start_log_block()
         tsr = etree.Element('tsRequest')
-
         url = self.build_api_url('tasks/extractRefreshes/{}/runNow'.format(task_luid))
         response = self.send_add_request(url, tsr)
         self.end_log_block()
         return response.findall('.//t:job', self.ns_map)[0].get("id")
 
-    def run_all_extract_refreshes_for_schedule(self, schedule_name_or_luid):
-        """
-        :type schedule_name_or_luid: unicode
-        :return:
-        """
+    def run_all_extract_refreshes_for_schedule(self, schedule_name_or_luid: str):
         self.start_log_block()
         extracts = self.query_extract_refresh_tasks_by_schedule(schedule_name_or_luid)
         for extract in extracts:
             self.run_extract_refresh_task(extract.get('id'))
         self.end_log_block()
 
-    def run_extract_refresh_for_workbook(self, wb_name_or_luid, proj_name_or_luid=None, username_or_luid=None):
-        """
-        :type wb_name_or_luid: unicode
-        :type proj_name_or_luid: unicode
-        :type username_or_luid: unicode
-        :return:
-        """
+    def run_extract_refresh_for_workbook(self, wb_name_or_luid: str, proj_name_or_luid: Optional[str] = None):
         self.start_log_block()
-        if self.is_luid(wb_name_or_luid):
-            wb_luid = wb_name_or_luid
-        else:
-            wb_luid = self.query_workbook_luid(wb_name_or_luid, proj_name_or_luid, username_or_luid)
+
+        wb_luid = self.query_workbook_luid(wb_name_or_luid, proj_name_or_luid)
         tasks = self.get_extract_refresh_tasks()
 
         extracts_for_wb = tasks.findall('.//t:extract/workbook[@id="{}"]..'.format(wb_luid), self.ns_map)
@@ -81,22 +51,11 @@ class ExtractMethods(TableauRestApiBase):
             self.run_extract_refresh_task(extract.get('id'))
         self.end_log_block()
 
-        # Check if this actually works
-
-    def run_extract_refresh_for_datasource(self, ds_name_or_luid, proj_name_or_luid=None):
-        """
-        :type ds_name_or_luid: unicode
-        :type proj_name_or_luid: unicode
-        :type username_or_luid: unicode
-        :return:
-        """
+    # Check if this actually works
+    def run_extract_refresh_for_datasource(self, ds_name_or_luid: str, proj_name_or_luid: Optional[str] = None):
         self.start_log_block()
-        if self.is_luid(ds_name_or_luid):
-            ds_luid = ds_name_or_luid
-        else:
-            ds_luid = self.query_datasource_luid(ds_name_or_luid, proj_name_or_luid)
+        ds_luid = self.query_datasource_luid(ds_name_or_luid, proj_name_or_luid)
         tasks = self.get_extract_refresh_tasks()
-        print(tasks)
         extracts_for_ds = tasks.findall('.//t:extract/datasource[@id="{}"]..'.format(ds_luid), self.ns_map)
         # print extracts_for_wb
         for extract in extracts_for_ds:
@@ -104,11 +63,7 @@ class ExtractMethods(TableauRestApiBase):
         self.end_log_block()
 
     # Checks status of AD sync process or extract
-    def query_job(self, job_luid):
-        """
-        :type job_luid: unicode
-        :rtype: etree.Element
-        """
+    def query_job(self, job_luid: str) -> etree.Element:
         self.start_log_block()
         job = self.query_resource("jobs/{}".format(job_luid))
         self.end_log_block()
@@ -118,17 +73,11 @@ class ExtractMethods27(ExtractMethods):
     pass
 
 class ExtractMethods28(ExtractMethods27):
-    def update_datasource_now(self, ds_name_or_luid, project_name_or_luid=False):
-        """
-        :type ds_name_or_luid: unicode
-        :type project_name_or_luid: unicode
-        :rtype: etree.Element
-        """
+    def update_datasource_now(self, ds_name_or_luid: str,
+                              project_name_or_luid: Optional[str] = None) -> etree.Element:
+
         self.start_log_block()
-        if self.is_luid(ds_name_or_luid) is False:
-            ds_luid = self.query_datasource_luid(ds_name_or_luid, project_name_or_luid=project_name_or_luid)
-        else:
-            ds_luid = ds_name_or_luid
+        ds_luid = self.query_datasource_luid(ds_name_or_luid, project_name_or_luid=project_name_or_luid)
 
         # Has an empty request but is POST because it makes a
         tsr = etree.Element('tsRequest')
@@ -139,17 +88,9 @@ class ExtractMethods28(ExtractMethods27):
         self.end_log_block()
         return response
 
-    def update_workbook_now(self, wb_name_or_luid, project_name_or_luid=False):
-        """
-        :type wb_name_or_luid: unicode
-        :type project_name_or_luid: unicode
-        :rtype: etree.Element
-        """
+    def update_workbook_now(self, wb_name_or_luid: str, project_name_or_luid: Optional[str] = None) -> etree.Element:
         self.start_log_block()
-        if self.is_luid(wb_name_or_luid) is False:
-            wb_luid = self.query_workbook_luid(wb_name_or_luid, proj_name_or_luid=project_name_or_luid)
-        else:
-            wb_luid = wb_name_or_luid
+        wb_luid = self.query_workbook_luid(wb_name_or_luid, proj_name_or_luid=project_name_or_luid)
 
         # Has an empty request but is POST because it makes a
         tsr = etree.Element('tsRequest')
@@ -160,22 +101,13 @@ class ExtractMethods28(ExtractMethods27):
         self.end_log_block()
         return response
 
-    def run_extract_refresh_for_workbook(self, wb_name_or_luid, proj_name_or_luid=None, username_or_luid=None):
-        """
-        :type wb_name_or_luid: unicode
-        :type proj_name_or_luid: unicode
-        :type username_or_luid: unicode
-        :return:
-        """
+    def run_extract_refresh_for_workbook(self, wb_name_or_luid: str,
+                                         proj_name_or_luid: Optional[str] = None) -> etree.Element:
         return self.update_workbook_now(wb_name_or_luid, proj_name_or_luid)
 
     # Use the specific refresh rather than the schedule task in 2.8
-    def run_extract_refresh_for_datasource(self, ds_name_or_luid, proj_name_or_luid=None):
-        """
-        :type ds_name_or_luid: unicode
-        :type proj_name_or_luid: unicode
-        :rtype: etree.Element
-        """
+    def run_extract_refresh_for_datasource(self, ds_name_or_luid: str,
+                                           proj_name_or_luid: Optional[str] = None) -> etree.Element:
         return self.update_datasource_now(ds_name_or_luid, proj_name_or_luid)
 
 
@@ -183,19 +115,11 @@ class ExtractMethods30(ExtractMethods28):
     pass
 
 class ExtractMethods31(ExtractMethods30):
-    def query_jobs(self, progress_filter=None, job_type_filter=None, created_at_filter=None, started_at_filter=None,
-                   ended_at_filter=None, title_filter=None, subtitle_filter=None, notes_filter=None):
-        """
-        :type progress_filter: UrlFilter
-        :type job_type_filter: UrlFilter
-        :type created_at_filter: UrlFilter
-        :type started_at_filter: UrlFilter
-        :type title_filter: UrlFilter
-        :type notes_filter: UrlFilter
-        :type subtitle_filter: UrlFilter
-        :type ended_at_filter: UrlFilter
-        :rtype: etree.Element
-        """
+    def query_jobs(self, progress_filter: Optional[UrlFilter] = None, job_type_filter: Optional[UrlFilter] = None,
+                   created_at_filter: Optional[UrlFilter] = None, started_at_filter: Optional[UrlFilter] = None,
+                   ended_at_filter: Optional[UrlFilter] = None, title_filter: Optional[UrlFilter] = None,
+                   subtitle_filter: Optional[UrlFilter] = None,
+                   notes_filter: Optional[UrlFilter] = None) -> etree.Element:
         self.start_log_block()
         filter_checks = {'progress': progress_filter, 'jobType': job_type_filter,
                          'createdAt': created_at_filter, 'title': title_filter,
@@ -208,11 +132,7 @@ class ExtractMethods31(ExtractMethods30):
         self.end_log_block()
         return jobs
 
-    def cancel_job(self, job_luid):
-        """
-        :type job_luid: unicode
-        :return:
-        """
+    def cancel_job(self, job_luid: str):
         self.start_log_block()
         url = self.build_api_url("jobs/{}".format(job_luid))
         self.send_update_request(url, None)
--- tableau_rest_api/tableau_rest_api_connection.py
+++ tableau_rest_api/tableau_rest_api_connection.py
@@ -1,6 +1,7 @@
 from .methods import *
 
-# This is a now a composite class that brings together all the different methods under one roof
+# This is a now a composite class that brings together all the different methods under one roof, but compatible with
+# older scripts (easy upgrade)
 class TableauRestApiConnection(WorkbookMethods, UserMethods, SiteMethods, ServerMethods,
                                ScheduleMethods, RevisionMethods, ProjectMethods, GroupMethods, FavoritesMethods,
                                ExtractMethods, DatasourceMethods, TableauRestApiBase):
