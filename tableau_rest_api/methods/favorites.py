4e86d886557a78ccb3d29dc32330406b87884080
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-11-11 16:37:02 -0600

    Type hinted Favorites methods

--- tableau_rest_api/methods/favorites.py
+++ tableau_rest_api/methods/favorites.py
@@ -52,100 +52,58 @@ class FavoritesMethods(TableauRestApiBase):
     # End Add methods
     #
 
-    def query_user_favorites(self, username_or_luid):
-        """
-        :type username_or_luid: unicode
-        :rtype: etree.Element
-        """
+    def query_user_favorites(self, username_or_luid: str) -> etree.Element:
+
         self.start_log_block()
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+
+        user_luid = self.query_user_luid(username_or_luid)
         favorites = self.query_resource("favorites/{}/".format(user_luid))
 
         self.end_log_block()
         return favorites
 
-    def query_user_favorites_json(self, username_or_luid, page_number=None):
-        """
-        :type username_or_luid: unicode
-        :rtype: json
-        """
+    def query_user_favorites_json(self, username_or_luid: str, page_number: Optional[int] = None) -> str:
         self.start_log_block()
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+
+        user_luid = self.query_user_luid(username_or_luid)
         favorites = self.query_resource_json("favorites/{}/".format(user_luid), page_number=page_number)
 
         self.end_log_block()
         return favorites
 
     # Can take collection or luid_string
-    def delete_workbooks_from_user_favorites(self, wb_name_or_luid_s, username_or_luid):
-        """
-        :type wb_name_or_luid_s: List[unicode] or unicode
-        :type username_or_luid: unicode
-        :rtype:
-        """
+    def delete_workbooks_from_user_favorites(self, wb_name_or_luid_s: Union[List[str], str],
+                                             username_or_luid: str):
         self.start_log_block()
         wbs = self.to_list(wb_name_or_luid_s)
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+        user_luid = self.query_user_luid(username_or_luid)
         for wb in wbs:
-            if self.is_luid(wb):
-                wb_luid = wb
-            else:
-                wb_luid = self.query_workbook_luid(wb)
+            wb_luid = self.query_workbook_luid(wb)
             url = self.build_api_url("favorites/{}/workbooks/{}".format(user_luid, wb_luid))
             self.send_delete_request(url)
         self.end_log_block()
 
-    def delete_views_from_user_favorites(self, view_name_or_luid_s, username_or_luid, wb_name_or_luid=None):
-        """
-        :type view_name_or_luid_s: List[unicode] or unicode
-        :type username_or_luid: unicode
-        :type wb_name_or_luid: unicode
-        :rtype:
-        """
+    def delete_views_from_user_favorites(self, view_name_or_luid_s: Union[List[str], str],
+                                         username_or_luid: str, wb_name_or_luid: Optional[str] = None):
         self.start_log_block()
         views = self.to_list(view_name_or_luid_s)
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+
+        user_luid = self.query_user_luid(username_or_luid)
         for view in views:
-            if self.is_luid(view):
-                view_luid = view
-            else:
-                view_luid = self.query_workbook_view_luid(wb_name_or_luid, view)
+
+            view_luid = self.query_workbook_view_luid(wb_name_or_luid, view)
             url = self.build_api_url("favorites/{}/views/{}".format(user_luid, view_luid))
             self.send_delete_request(url)
         self.end_log_block()
 
-    def add_datasource_to_user_favorites(self, favorite_name, ds_name_or_luid_s, username_or_luid, p_name_or_luid=None):
-        """
-        :type favorite_name: unicode
-        :type ds_name_or_luid_s: unicode
-        :type username_or_luid: unicode
-        :type p_name_or_luid: unicode
-        :rtype:
-        """
+    def add_datasource_to_user_favorites(self, favorite_name: str, ds_name_or_luid_s: Union[List[str], str],
+                                         username_or_luid: str, p_name_or_luid: Optional[str] = None):
         self.start_log_block()
         dses = self.to_list(ds_name_or_luid_s)
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+        user_luid = self.query_user_luid(username_or_luid)
 
         for ds in dses:
-            if self.is_luid(ds_name_or_luid_s):
-                datasource_luid = ds
-            else:
-                datasource_luid = self.query_datasource_luid(ds, p_name_or_luid)
+            datasource_luid = self.query_datasource_luid(ds, p_name_or_luid)
 
             tsr = etree.Element('tsRequest')
             f = etree.Element('favorite')
@@ -160,24 +118,13 @@ class FavoritesMethods(TableauRestApiBase):
 
         self.end_log_block()
 
-    def delete_datasources_from_user_favorites(self, ds_name_or_luid_s, username_or_luid, p_name_or_luid=None):
-        """
-        :type ds_name_or_luid_s: List[unicode] or unicode
-        :type username_or_luid: unicode
-        :type p_name_or_luid: unicode
-        :rtype:
-        """
+    def delete_datasources_from_user_favorites(self, ds_name_or_luid_s: Union[List[str], str],
+                                               username_or_luid: str, p_name_or_luid: Optional[str] = None):
         self.start_log_block()
         dses = self.to_list(ds_name_or_luid_s)
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+        user_luid = self.query_user_luid(username_or_luid)
         for ds in dses:
-            if self.is_luid(ds):
-                ds_luid = ds
-            else:
-                ds_luid = self.query_datasource_luid(ds, p_name_or_luid)
+            ds_luid = self.query_datasource_luid(ds, p_name_or_luid)
             url = self.build_api_url("favorites/{}/datasources/{}".format(user_luid, ds_luid))
             self.send_delete_request(url)
         self.end_log_block()
@@ -192,17 +139,9 @@ class FavoritesMethods30(FavoritesMethods28):
     pass
 
 class FavoritesMethods31(FavoritesMethods30):
-    def add_project_to_user_favorites(self, favorite_name, proj_name_or_luid=None):
-        """
-        :type favorite_name: unicode
-        :type proj_name_or_luid: unicode
-        :rtype: etree.Element
-        """
+    def add_project_to_user_favorites(self, favorite_name: str, proj_name_or_luid: str) -> etree.Element:
         self.start_log_block()
-        if self.is_luid(proj_name_or_luid):
-            proj_luid = proj_name_or_luid
-        else:
-            proj_luid = self.query_project_luid(proj_name_or_luid)
+        proj_luid = self.query_project_luid(proj_name_or_luid)
 
         tsr = etree.Element('tsRequest')
         f = etree.Element('favorite')
@@ -217,23 +156,13 @@ class FavoritesMethods31(FavoritesMethods30):
         self.end_log_block()
         return update_response
 
-    def delete_projects_from_user_favorites(self, proj_name_or_luid_s, username_or_luid):
-        """
-        :type proj_name_or_luid_s: list[unicode] or unicode
-        :type username_or_luid: unicode
-        :rtype:
-        """
+    def delete_projects_from_user_favorites(self, proj_name_or_luid_s: Union[List[str], str],
+                                            username_or_luid:str):
         self.start_log_block()
         projs = self.to_list(proj_name_or_luid_s)
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+        user_luid = self.query_user_luid(username_or_luid)
         for proj in projs:
-            if self.is_luid(proj):
-                proj_luid = proj
-            else:
-                proj_luid = self.query_project_luid(proj)
+            proj_luid = self.query_project_luid(proj)
             url = self.build_api_url("favorites/{}/projects/{}".format(user_luid, proj_luid))
             self.send_delete_request(url)
         self.end_log_block()
