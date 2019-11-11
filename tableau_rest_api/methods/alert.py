8eaba1d7b5f711173370e8609c9b9f5fb32c7761
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-11-11 16:15:41 -0600

    Alerts methods modern type hinting

--- tableau_rest_api/methods/alert.py
+++ tableau_rest_api/methods/alert.py
@@ -2,36 +2,33 @@ from .rest_api_base import *
 
 # First Alert Methods appear in API 3.2
 class AlertMethods32(TableauRestApiBase32):
-    def query_data_driven_alerts(self):
+    def query_data_driven_alerts(self) -> etree.Element:
         self.start_log_block()
         alerts = self.query_resource("dataAlerts")
         self.end_log_block()
         return alerts
 
-    def query_data_driven_alerts_for_view(self, view_luid):
+    def query_data_driven_alerts_for_view(self, view_luid: str) -> etree.Element:
         self.start_log_block()
         alerts = self.query_resource("dataAlerts?filter=viewId:eq:{}".format(view_luid))
         self.end_log_block()
         return alerts
 
-    def query_data_driven_alert_details(self, data_alert_luid):
+    def query_data_driven_alert_details(self, data_alert_luid: str) -> etree.Element:
         self.start_log_block()
         alert_details = self.query_resource("dataAlerts/{}".format(data_alert_luid))
         self.end_log_block()
         return alert_details
 
-    def delete_data_driven_alert(self, data_alert_luid):
+    def delete_data_driven_alert(self, data_alert_luid: str):
         self.start_log_block()
         url = self.build_api_url("dataAlerts/{}".format(data_alert_luid))
         self.send_delete_request(url)
         self.end_log_block()
 
-    def add_user_to_data_driven_alert(self, data_alert_luid, username_or_luid):
+    def add_user_to_data_driven_alert(self, data_alert_luid: str, username_or_luid: str):
         self.start_log_block()
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+        user_luid = self.query_user_luid(username_or_luid)
 
         tsr = etree.Element("tsRequest")
         u = etree.Element("user")
@@ -41,13 +38,9 @@ class AlertMethods32(TableauRestApiBase32):
         self.send_add_request(url, tsr)
         self.end_log_block()
 
-    def update_data_driven_alert(self, data_alert_luid, subject=None, frequency=None, owner_username_or_luid=None):
-        """
-        :type subject: unicode
-        :type frequency: unicode
-        :type owner_username_or_luid: unicode
-        :return:
-        """
+    def update_data_driven_alert(self, data_alert_luid: str, subject: Optional[str] = None,
+                                 frequency: Optional[str] = None,
+                                 owner_username_or_luid: Optional[str] = None) -> etree.Element:
         self.start_log_block()
         tsr = etree.Element("tsRequest")
         d = etree.Element("dataAlert")
@@ -62,10 +55,7 @@ class AlertMethods32(TableauRestApiBase32):
             d.set('frequency', frequency)
 
         if owner_username_or_luid is not None:
-            if self.is_luid(owner_username_or_luid):
-                owner_luid = owner_username_or_luid
-            else:
-                owner_luid = self.query_user_luid(owner_username_or_luid)
+            owner_luid = self.query_user_luid(owner_username_or_luid)
             o = etree.Element('owner')
             o.set("id", owner_luid)
             d.append(o)
@@ -76,12 +66,9 @@ class AlertMethods32(TableauRestApiBase32):
         self.end_log_block()
         return response
 
-    def delete_user_from_data_driven_alert(self, data_alert_luid, username_or_luid):
+    def delete_user_from_data_driven_alert(self, data_alert_luid: str, username_or_luid: str):
         self.start_log_block()
-        if self.is_luid(username_or_luid):
-            user_luid = username_or_luid
-        else:
-            user_luid = self.query_user_luid(username_or_luid)
+        user_luid = self.query_user_luid(username_or_luid)
 
         url = self.build_api_url('dataAlerts/{}/users/{}'.format(data_alert_luid, user_luid))
         self.send_delete_request(url)
