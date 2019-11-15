7033bd23fec61e3d890c35597a5c894eab7af056
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-11-15 12:40:18 -0600

    Implement PDF and Image subscription parameters on Create and Update subscription

--- tableau_rest_api/methods/subscription.py
+++ tableau_rest_api/methods/subscription.py
@@ -191,6 +191,89 @@ class SubscriptionMethods35(SubscriptionMethods34):
     def __init__(self, rest_api_base: TableauRestApiBase35):
         self.rest_api_base = rest_api_base
 
+    def create_subscription(self, subscription_subject: Optional[str] = None, view_or_workbook: Optional[str] = None,
+                            content_name_or_luid: Optional[str] = None, schedule_name_or_luid: Optional[str] = None,
+                            username_or_luid: Optional[str] = None, project_name_or_luid: Optional[str] = None,
+                            wb_name_or_luid: Optional[str] = None,
+                            image_attachment: bool = True, pdf_attachment: bool = False,
+                            direct_xml_request: Optional[ET.Element] = None) -> str:
+        self.start_log_block()
+        if direct_xml_request is not None:
+            tsr = direct_xml_request
+        else:
+            if view_or_workbook not in ['View', 'Workbook']:
+                raise InvalidOptionException("view_or_workbook must be 'Workbook' or 'View'")
+
+            user_luid = self.query_user_luid(username_or_luid)
+            schedule_luid = self.query_schedule_luid(schedule_name_or_luid)
+
+            if self.is_luid(content_name_or_luid):
+                content_luid = content_name_or_luid
+            else:
+                if view_or_workbook == 'View':
+                    if wb_name_or_luid is None:
+                        raise InvalidOptionException('Must include wb_name_or_luid for a View name lookup')
+                    content_luid = self.query_workbook_view_luid(wb_name_or_luid, content_name_or_luid,
+                                                                 proj_name_or_luid=project_name_or_luid, username_or_luid=user_luid)
+                elif view_or_workbook == 'Workbook':
+                    content_luid = self.query_workbook_luid(content_name_or_luid, project_name_or_luid, user_luid)
+                else:
+                    raise InvalidOptionException("view_or_workbook must be 'Workbook' or 'View'")
+
+            tsr = ET.Element('tsRequest')
+            s = ET.Element('subscription')
+            s.set('subject', subscription_subject)
+            s.set('attachImage', str(image_attachment).lower())
+            s.set('attachPdf', str(pdf_attachment).lower())
+
+            c = ET.Element('content')
+            c.set('type', view_or_workbook)
+            c.set('id', content_luid)
+            sch = ET.Element('schedule')
+            sch.set('id', schedule_luid)
+            u = ET.Element('user')
+            u.set('id', user_luid)
+            s.append(c)
+            s.append(sch)
+            s.append(u)
+            tsr.append(s)
+
+        url = self.build_api_url('subscriptions')
+        try:
+            new_subscription = self.send_add_request(url, tsr)
+            new_subscription_luid = new_subscription.findall('.//t:subscription', self.ns_map)[0].get("id")
+            self.end_log_block()
+            return new_subscription_luid
+        except RecoverableHTTPException as e:
+            self.end_log_block()
+            raise e
+
+    def update_subscription(self, subscription_luid: str, subject: Optional[str] = None,
+                            schedule_luid: Optional[str] = None, image_attachment: Optional[bool] = None,
+                            pdf_attachment: Optional[bool] = None) -> ET.Element:
+        if subject is None and schedule_luid is None:
+            raise InvalidOptionException("You must pass one of subject or schedule_luid, or both")
+        tsr = ET.Element('tsRequest')
+        s = ET.Element('subscription')
+
+        if subject is not None:
+            s.set('subject', subject)
+        if image_attachment is not None:
+            s.set('attachImage', str(image_attachment).lower())
+        if pdf_attachment is not None:
+            s.set('attachPdf', str(pdf_attachment).lower())
+
+        if schedule_luid is not None:
+            sch = ET.Element('schedule')
+            sch.set('id', schedule_luid)
+            s.append(sch)
+        tsr.append(s)
+
+        url = self.build_api_url("subscriptions/{}".format(subscription_luid))
+        response = self.send_update_request(url, tsr)
+        self.end_log_block()
+        return response
+
 class SubscriptionMethods36(SubscriptionMethods35):
     def __init__(self, rest_api_base: TableauRestApiBase36):
         self.rest_api_base = rest_api_base
