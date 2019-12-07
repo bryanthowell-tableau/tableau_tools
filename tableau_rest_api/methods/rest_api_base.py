229fb36463ab9d9e5dee13a0276f7ff3b346d073
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-12-07 09:44:23 -0600

    Added a .url_filters property to rest_api_base as place for hitting the factory methods

--- tableau_rest_api/methods/rest_api_base.py
+++ tableau_rest_api/methods/rest_api_base.py
@@ -46,6 +46,9 @@ class TableauRestApiBase(LookupMethods, LoggingMethods):
         self._request_obj: Optional[RestXmlRequest] = None
         self._request_json_obj: Optional[RestJsonRequest] = None
 
+        # UrlFilter object for factory methods
+        self.url_filters = UrlFilter()
+
         # Lookup caches to minimize calls
         self.username_luid_cache = {}
         self.group_name_luid_cache = {}
@@ -1158,6 +1161,7 @@ class TableauRestApiBase27(TableauRestApiBase):
         TableauRestApiBase.__init__(self, server=server, username=username, password=password,
                                     site_content_url=site_content_url)
         self.set_tableau_server_version('10.4')
+        self.url_filters = UrlFilter27()
 
 class TableauRestApiBase28(TableauRestApiBase27):
     def __init__(self, server: str, username: str, password: str, site_content_url: Optional[str] = ""):
@@ -1184,6 +1188,7 @@ class TableauRestApiBase31(TableauRestApiBase30):
         TableauRestApiBase.__init__(self, server=server, username=username, password=password,
                                     site_content_url=site_content_url)
         self.set_tableau_server_version('2018.2')
+        self.url_filters = UrlFilter31()
 
 class TableauRestApiBase32(TableauRestApiBase31):
     def __init__(self, server: str, username: str, password: str, site_content_url: Optional[str] = ""):
@@ -1196,6 +1201,7 @@ class TableauRestApiBase33(TableauRestApiBase32):
         TableauRestApiBase.__init__(self, server=server, username=username, password=password,
                                     site_content_url=site_content_url)
         self.set_tableau_server_version('2019.1')
+        self.url_filters = UrlFilter33()
 
     def get_published_project_object(self, project_name_or_luid: str,
                                      project_xml_obj: Optional[ET.Element] = None) -> Project33:
