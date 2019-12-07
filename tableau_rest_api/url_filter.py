0bccf4771ef7cee817c68634d56557c0afec40a4
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-12-07 09:41:11 -0600

    Renamed the factory methods on UrlFilter from "create" to "get" to indicate the object will be returned

--- tableau_rest_api/url_filter.py
+++ tableau_rest_api/url_filter.py
@@ -20,12 +20,12 @@ class UrlFilter:
 
     # Users, Datasources, Views, Workbooks
     @staticmethod
-    def create_name_filter(name: str) -> 'UrlFilter':
+    def get_name_filter(name: str) -> 'UrlFilter':
         return UrlFilter('name', 'eq', [name, ])
 
     # Users
     @staticmethod
-    def create_last_login_filter(operator: str, last_login_time: str) -> 'UrlFilter':
+    def get_last_login_filter(operator: str, last_login_time: str) -> 'UrlFilter':
         """
         :param operator: Should be one of 'eq', 'gt', 'gte', 'lt', 'lte'
         :param last_login_time: ISO 8601 representation of time like 2016-01-01T00:00:00:00Z
@@ -39,18 +39,18 @@ class UrlFilter:
 
     # Users
     @staticmethod
-    def create_site_role_filter(site_role: str) -> 'UrlFilter':
+    def get_site_role_filter(site_role: str) -> 'UrlFilter':
         return UrlFilter('siteRole', 'eq', [site_role, ])
 
     # Workbooks
     @staticmethod
-    def create_owner_name_filter(owner_name: str) -> 'UrlFilter':
+    def get_owner_name_filter(owner_name: str) -> 'UrlFilter':
 
         return UrlFilter('ownerName', 'eq', [owner_name, ])
 
     # Workbooks, Datasources, Views, Jobs
     @staticmethod
-    def create_created_at_filter(operator: str, created_at_time: str) -> 'UrlFilter':
+    def get_created_at_filter(operator: str, created_at_time: str) -> 'UrlFilter':
         """
         :param operator: Should be one of 'eq', 'gt', 'gte', 'lt', 'lte'
         :param created_at_time: ISO 8601 representation of time like 2016-01-01T00:00:00:00Z
@@ -64,7 +64,7 @@ class UrlFilter:
 
     # Workbooks, Datasources, Views
     @staticmethod
-    def create_updated_at_filter(operator: str, updated_at_time: str) -> 'UrlFilter':
+    def get_updated_at_filter(operator: str, updated_at_time: str) -> 'UrlFilter':
         """
         :param operator: Should be one of 'eq', 'gt', 'gte', 'lt', 'lte'
         :param updated_at_time: ISO 8601 representation of time like 2016-01-01T00:00:00:00Z
@@ -78,17 +78,17 @@ class UrlFilter:
 
     # Workbooks, Datasources, Views
     @staticmethod
-    def create_tags_filter(tags: List[str]) -> 'UrlFilter':
+    def get_tags_filter(tags: List[str]) -> 'UrlFilter':
         return UrlFilter('tags', 'in', tags)
 
     # Workbooks, Datasources, Views
     @staticmethod
-    def create_tag_filter(tag: str) -> 'UrlFilter':
+    def get_tag_filter(tag: str) -> 'UrlFilter':
         return UrlFilter('tags', 'eq', [tag, ])
 
     # Datasources
     @staticmethod
-    def create_datasource_type_filter(ds_type: str) -> 'UrlFilter':
+    def get_datasource_type_filter(ds_type: str) -> 'UrlFilter':
         return UrlFilter('type', 'eq', [ds_type, ])
 
 class UrlFilter27(UrlFilter):
@@ -98,64 +98,64 @@ class UrlFilter27(UrlFilter):
 
     # Users, Datasources, Views, Workbooks
     @staticmethod
-    def create_names_filter(names: List[str]) -> 'UrlFilter':
+    def get_names_filter(names: List[str]) -> 'UrlFilter':
         return UrlFilter('name', 'in', names)
 
     # Users
     @staticmethod
-    def create_site_roles_filter(site_roles: List[str]) -> 'UrlFilter':
+    def get_site_roles_filter(site_roles: List[str]) -> 'UrlFilter':
         return UrlFilter('siteRole', 'in', site_roles)
 
     # Workbooks, Projects
     @staticmethod
-    def create_owner_names_filter(owner_names: List[str]) -> 'UrlFilter':
+    def get_owner_names_filter(owner_names: List[str]) -> 'UrlFilter':
         return UrlFilter('ownerName', 'in', owner_names)
 
     # Workbooks. Datasources, Views
-    # create_owner_name_filter (singular) allowed for all of them
+    # get_owner_name_filter (singular) allowed for all of them
 
     # Groups
     @staticmethod
-    def create_domain_names_filter(domain_names: List[str]) -> 'UrlFilter':
+    def get_domain_names_filter(domain_names: List[str]) -> 'UrlFilter':
         return UrlFilter('domainName', 'in', domain_names)
 
     # Groups
     @staticmethod
-    def create_domain_nicknames_filter(domain_nicknames: str) -> 'UrlFilter':
+    def get_domain_nicknames_filter(domain_nicknames: str) -> 'UrlFilter':
 
         return UrlFilter('domainNickname', 'in', domain_nicknames)
 
     # Groups
     @staticmethod
-    def create_domain_name_filter(domain_name: str) -> 'UrlFilter':
+    def get_domain_name_filter(domain_name: str) -> 'UrlFilter':
         return UrlFilter('domainName', 'eq', [domain_name, ])
 
     # Groups
     @staticmethod
-    def create_domain_nickname_filter(domain_nickname: str) -> 'UrlFilter':
+    def get_domain_nickname_filter(domain_nickname: str) -> 'UrlFilter':
 
         return UrlFilter('domainNickname', 'eq', [domain_nickname, ])
 
     # Groups
     @staticmethod
-    def create_minimum_site_roles_filter(minimum_site_roles: List[str]) -> 'UrlFilter':
+    def get_minimum_site_roles_filter(minimum_site_roles: List[str]) -> 'UrlFilter':
         return UrlFilter('minimumSiteRole', 'in', minimum_site_roles)
 
     # Groups
     @staticmethod
-    def create_minimum_site_role_filter(minimum_site_role: str) -> 'UrlFilter':
+    def get_minimum_site_role_filter(minimum_site_role: str) -> 'UrlFilter':
         return UrlFilter('minimumSiteRole', 'eq', [minimum_site_role, ])
 
     # Groups
     @staticmethod
-    def create_is_local_filter(is_local: bool) -> 'UrlFilter':
+    def get_is_local_filter(is_local: bool) -> 'UrlFilter':
         if is_local not in [True, False]:
             raise InvalidOptionException('is_local must be True or False')
         return UrlFilter('isLocal', 'eq', [str(is_local).lower(), ])
 
     # Groups
     @staticmethod
-    def create_user_count_filter(operator, user_count: int) -> 'UrlFilter':
+    def get_user_count_filter(operator, user_count: int) -> 'UrlFilter':
         comparison_operators = ['eq', 'gt', 'gte', 'lt', 'lte']
         if operator not in comparison_operators:
             raise InvalidOptionException("operator must be one of 'eq', 'gt', 'gte', 'lt', 'lte' ")
@@ -165,27 +165,27 @@ class UrlFilter27(UrlFilter):
 
     # Projects
     @staticmethod
-    def create_owner_domains_filter(owner_domains: List[str]) -> 'UrlFilter':
+    def get_owner_domains_filter(owner_domains: List[str]) -> 'UrlFilter':
         return UrlFilter('ownerDomain', 'in', owner_domains)
 
     # Projects
     @staticmethod
-    def create_owner_domain_filter(owner_domain: str) -> 'UrlFilter':
+    def get_owner_domain_filter(owner_domain: str) -> 'UrlFilter':
         return UrlFilter('ownerDomain', 'in', [owner_domain, ])
 
     # Projects
     @staticmethod
-    def create_owner_emails_filter(owner_emails: List[str]) -> 'UrlFilter':
+    def get_owner_emails_filter(owner_emails: List[str]) -> 'UrlFilter':
         return UrlFilter('ownerEmail', 'in', owner_emails)
 
     # Projects
     @staticmethod
-    def create_owner_email_filter(owner_email: str) -> 'UrlFilter':
+    def get_owner_email_filter(owner_email: str) -> 'UrlFilter':
         return UrlFilter('ownerEmail', 'in', [owner_email, ])
 
     # Views
     @staticmethod
-    def create_hits_total_filter(operator, hits_total: int) -> 'UrlFilter':
+    def get_hits_total_filter(operator, hits_total: int) -> 'UrlFilter':
         comparison_operators = ['eq', 'gt', 'gte', 'lt', 'lte']
         if operator not in comparison_operators:
             raise InvalidOptionException("operator must be one of 'eq', 'gt', 'gte', 'lt', 'lte' ")
@@ -212,7 +212,7 @@ class UrlFilter31(UrlFilter30):
 
     # Jobs
     @staticmethod
-    def create_started_at_filter(operator: str, started_at_time: str) -> UrlFilter:
+    def get_started_at_filter(operator: str, started_at_time: str) -> UrlFilter:
         """
         :param operator: Should be one of 'eq', 'gt', 'gte', 'lt', 'lte'
         :param started_at_time: ISO 8601 representation of time like 2016-01-01T00:00:00:00Z
@@ -226,7 +226,7 @@ class UrlFilter31(UrlFilter30):
 
     # Jobs
     @staticmethod
-    def create_ended_at_filter(operator: str, ended_at_time: str) -> UrlFilter:
+    def get_ended_at_filter(operator: str, ended_at_time: str) -> UrlFilter:
         """
         :param operator: Should be one of 'eq', 'gt', 'gte', 'lt', 'lte'
         :param ended_at_time: ISO 8601 representation of time like 2016-01-01T00:00:00:00Z
@@ -240,33 +240,33 @@ class UrlFilter31(UrlFilter30):
 
     # Jobs
     @staticmethod
-    def create_job_types_filter(job_types: List[str]) -> UrlFilter:
+    def get_job_types_filter(job_types: List[str]) -> UrlFilter:
         return UrlFilter('jobType', 'in', job_types)
 
     # Jobs
     @staticmethod
-    def create_job_type_filter(job_type: str) -> UrlFilter:
+    def get_job_type_filter(job_type: str) -> UrlFilter:
         return UrlFilter('tags', 'eq', [job_type, ])
 
     # Jobs
     @staticmethod
-    def create_notes_filter(notes: str) -> UrlFilter:
+    def get_notes_filter(notes: str) -> UrlFilter:
         return UrlFilter('notes', 'has', [notes, ])
 
     @staticmethod
-    def create_title_equals_filter(title: str) -> UrlFilter:
+    def get_title_equals_filter(title: str) -> UrlFilter:
         return UrlFilter('title', 'eq', [title, ])
 
     @staticmethod
-    def create_title_has_filter(title: str) -> UrlFilter:
+    def get_title_has_filter(title: str) -> UrlFilter:
         return UrlFilter('title', 'has', [title, ])
 
     @staticmethod
-    def create_subtitle_equals_filter(subtitle: str) -> UrlFilter:
+    def get_subtitle_equals_filter(subtitle: str) -> UrlFilter:
         return UrlFilter('subtitle', 'eq', [subtitle, ])
 
     @staticmethod
-    def create_subtitle_has_filter(subtitle: str) -> UrlFilter:
+    def get_subtitle_has_filter(subtitle: str) -> UrlFilter:
         return UrlFilter('subtitle', 'has', [subtitle, ])
 
 class UrlFilter33(UrlFilter31):
@@ -274,5 +274,5 @@ class UrlFilter33(UrlFilter31):
         UrlFilter31.__init__(self, field, operator, values)
 
     @staticmethod
-    def create_project_name_equals_filter(project_name: str) -> UrlFilter:
+    def get_project_name_equals_filter(project_name: str) -> UrlFilter:
         return UrlFilter('projectName', 'eq', [project_name, ])
