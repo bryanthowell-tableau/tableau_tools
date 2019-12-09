a3c2b67ba252d97b3a468c758f957102edf35550
Bryant Howell <bryantbhowell@users.noreply.github.com>
2019-12-09 07:35:10 -0600

    Updated to 5.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='tableau_tools',
     python_requires='>=3.6',
-    version='5.0.4',
+    version='5.0.5',
     packages=['tableau_tools', 'tableau_tools.tableau_rest_api', 'tableau_tools.tableau_documents', 'tableau_tools.examples'],
     url='https://github.com/bryantbhowell/tableau_tools',
     license='',
